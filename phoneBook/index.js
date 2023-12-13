6c731bc973eee6ba44b544f3963857a82a0c6321
Gigi <gigi151521@gmail.com>
2023-12-12 18:22:28 -0600

    "backEnd part3"

--- phoneBook/index.js
+++ phoneBook/index.js
@@ -115,7 +115,7 @@ app.post('/api/persons', morganPost, (request, response, next) => {
   }
 });
 
-const PORT = process.env.PORT || 3001;
+const PORT = 3001;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
