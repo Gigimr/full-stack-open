992dae595b3b93dc5e29e7c9805115b2b7a65d03
Gigi <gigi151521@gmail.com>
2024-01-16 17:54:13 -0600

    "Starting part4 and testing "

---
+++ part4/utils/list_helper.js
@@ -0,0 +1,29 @@
+const dummy = (blogs) => {
+  return 1;
+};
+
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => {
+    return sum + item;
+  };
+  const blogslikes = blogs.map((blog) => blog.likes);
+
+  return blogslikes.reduce(reducer, 0);
+};
+
+const favoriteBlog = (blogs) => {
+  const blogslikes = blogs.map((blog) => blog.likes);
+  const maxLikes = Math.max(...blogslikes);
+  const favInfo = blogs.find((blog) => blog.likes == maxLikes);
+  return {
+    title: favInfo.title,
+    author: favInfo.author,
+    likes: favInfo.likes,
+  };
+};
+
+module.exports = {
+  dummy,
+  totalLikes,
+  favoriteBlog,
+};
