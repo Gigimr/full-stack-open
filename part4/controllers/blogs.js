ce482b5ab2db32991b636ff14727549dd634feaf
Gigi <gigi151521@gmail.com>
2024-01-23 17:01:45 -0600

    adding delete functionality

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -41,5 +41,13 @@ blogsRouter.post('/', async (request, response) => {
     response.status(500).json({ error: 'Error saving the blog' });
   }
 });
+blogsRouter.delete('/:id', async (request, response, next) => {
+  try {
+    await Blog.findByIdAndDelete(request.params.id);
+    response.status(204).end;
+  } catch (exception) {
+    next(exception);
+  }
+});
 
 module.exports = blogsRouter;
