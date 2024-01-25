0c74a8d4dd8bd3d2a87ec2bae8324892eae69561
Gigi <gigi151521@gmail.com>
2024-01-25 10:15:49 -0600

    adding post test

--- part4/tests/blog_api.test.js
+++ part4/tests/blog_api.test.js
@@ -7,10 +7,10 @@ const Blog = require('../models/blog');
 
 beforeEach(async () => {
   await Blog.deleteMany({});
-  let blogObject = new Blog(helper.initialBlogs[0]);
-  await blogObject.save();
-  blogObject = new Blog(helper.initialBlogs[1]);
-  await blogObject.save();
+  for (let blog of helper.initialBlogs) {
+    let blogObject = new Blog(blog);
+    await blogObject.save();
+  }
 });
 
 test('blogs are returned as json', async () => {
@@ -21,10 +21,38 @@ test('blogs are returned as json', async () => {
 }, 100000);
 test('all blogs are returned', async () => {
   const response = await api.get('/api/blogs');
-  expect(response.body).toHaveLength(initialBlogs.length);
+  expect(response.body).toHaveLength(helper.initialBlogs.length);
 });
 
-// test('');
+test('a specific blog can be viewed by id', async () => {
+  const blogsAtStart = await helper.blogInDb();
+  const blogToView = blogsAtStart[0];
+
+  const resultBlog = await api
+    .get(`/api/blogs/${blogToView.id}`)
+    .expect(200)
+    .expect('Content-Type', /application\/json/);
+
+  expect(resultBlog).toBeDefined();
+});
+
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'TDD harms architecture',
+    author: 'Robert C. Martin',
+    url: 'http://blog.cleancoder.com/uncle-bob/2017/03/03/TDD-Harms-Architecture.html',
+    likes: 1,
+  };
+
+  await api
+    .post('/api/blogs/')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/);
+
+  const blogsAtEnd = await helper.blogInDb();
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1);
+});
 
 afterAll(async () => {
   await mongoose.connection.close();
