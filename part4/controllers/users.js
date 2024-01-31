ebb5f7020251447e853790994f6e43926b96adc2
Gigi <gigi151521@gmail.com>
2024-01-31 16:10:18 -0600

    adding user validations

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -9,19 +9,28 @@ usersRouter.get('/', async (request, response) => {
 
 usersRouter.post('/', async (request, response) => {
   const body = request.body;
+  if (!body.username || !body.password) {
+    return response
+      .status(400)
+      .json({ error: 'username and password must be given' });
+  }
+  if (body.username.length < 3 || body.password.length < 3) {
+    return response.status(400).json({
+      error: 'username and password must be at least 3 characters long',
+    });
+  } else {
+    const saltRounds = 10;
+    const passwordHash = await bcrypt.hash(body.password, saltRounds);
 
-  const saltRounds = 10;
-  const passwordHash = await bcrypt.hash(body.password, saltRounds);
+    const user = new User({
+      username: body.username,
+      name: body.name,
+      passwordHash,
+    });
+    const savedUser = await user.save();
 
-  const user = new User({
-    username: body.username,
-    name: body.name,
-    passwordHash,
-  });
-
-  const savedUser = await user.save();
-
-  response.json(savedUser);
+    response.json(savedUser);
+  }
 });
 
 module.exports = usersRouter;
