f9017d305dde29a38cae5ce069c6250f0d17dc24
Gigi <gigi151521@gmail.com>
2024-02-06 10:24:11 -0600

    adding user refrence to blogs

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -1,5 +1,6 @@
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
+const User = require('../models/user');
 
 blogsRouter.get('/', async (request, response) => {
   try {
@@ -27,18 +28,24 @@ blogsRouter.get('/:id', async (request, response) => {
 blogsRouter.post('/', async (request, response) => {
   const body = request.body;
 
+  const user = await User.findById(body.userId);
+
   const blog = new Blog({
     title: body.title,
     author: body.author,
     url: body.url,
     likes: body.likes ? body.likes : 0,
+    user: user._id,
   });
 
   if (body.title === undefined || body.url === undefined) {
     return response.status(400).json({ error: 'token missing or invalid ' });
   } else {
-    const result = await blog.save();
-    response.status(201).json(result);
+    const savedBlog = await blog.save();
+    user.blogs = user.blogs.concat(savedBlog._id);
+    await user.save();
+
+    response.json(savedBlog);
   }
 });
 blogsRouter.delete('/:id', async (request, response, next) => {
