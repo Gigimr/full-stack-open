a3eff16c3e6aa43a53dc756539b2653cf32e4abd
Gigi <gigi151521@gmail.com>
2024-03-07 14:27:18 -0600

    adding renders content test

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -17,12 +17,5 @@ test('renders content', () => {
   const div = container.querySelector('.unvisible');
 
   expect(div).toHaveTextContent('Component testing is done with react-testing-library John');
-//   expect(div).toBeInTheDocument();
 
-//   expect(div).toHaveTextContent(blog.title);
-//   expect(div).toHaveTextContent(blog.author);
-
-  //  URL y los likes no se muestren
-  // expect(div).not.toHaveTextContent(blog.url);
-  // expect(div).not.toHaveTextContent(`${blog.likes} likes`);
 });
