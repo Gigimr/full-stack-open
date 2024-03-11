13f01b0ef1a7792aeb58df0733cd797623c5dbf3
Gigi <gigi151521@gmail.com>
2024-03-11 16:57:11 -0600

    preparing the frontEnd- part5 test environment

--- part4/app.js
+++ part4/app.js
@@ -8,7 +8,7 @@ const logger = require('./utils/logger');
 const mongoose = require('mongoose');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
-// logger.info('connecting to', config.MONGODB_URI);
+//logger.info('connecting to ', config.MONGODB_URI);
 
 mongoose
   .connect(config.MONGODB_URI)
@@ -31,6 +31,10 @@ app.use('/api/users', usersRouter);
 app.use(middleware.tokenExtractor);
 // use the middleware only in /api/blogs routes
 app.use('/api/blogs', middleware.userExtractor, blogsRouter);
+if (process.env.NODE_ENV === 'test') {
+  const testingRouter = require('./controllers/testing')
+  app.use('/api/testing', testingRouter)
+}
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
