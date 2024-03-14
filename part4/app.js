926a1654d601ec63230a26341d6e2e611758095f
Gigi <gigi151521@gmail.com>
2024-03-14 10:42:17 -0600

    refactoring backend/app

--- part4/app.js
+++ part4/app.js
@@ -1,13 +1,17 @@
-const express = require('express');
 const config = require('./utils/config');
+const express = require('express')
 const app = express();
+
 const cors = require('cors');
 const blogsRouter = require('./controllers/blogs');
+const usersRouter = require('./controllers/users');
+const loginRouter = require('./controllers/login');
+
 const middleware = require('./utils/middleware');
 const logger = require('./utils/logger');
 const mongoose = require('mongoose');
-const usersRouter = require('./controllers/users');
-const loginRouter = require('./controllers/login');
+
+mongoose.set('strictQuery', false)
 //logger.info('connecting to ', config.MONGODB_URI);
 
 mongoose
@@ -19,22 +23,24 @@ mongoose
     logger.error('error connecting to MongoDB:', error.message);
   });
 
-app.use(cors());
-app.use(express.json());
-app.use(middleware.requestLogger);
-
-//public routes
-app.use('/api/login', loginRouter);
-app.use('/api/users', usersRouter);
-
-//private routes
-app.use(middleware.tokenExtractor);
-// use the middleware only in /api/blogs routes
-app.use('/api/blogs', middleware.userExtractor, blogsRouter);
-if (process.env.NODE_ENV === 'test') {
-  const testingRouter = require('./controllers/testing')
-  app.use('/api/testing', testingRouter)
-}
+  app.use(cors());
+  app.use(express.json());
+  app.use(middleware.requestLogger);
+  
+  //public routes
+  app.use('/api/login', loginRouter);
+  if (process.env.NODE_ENV === 'test') {
+    const testingRouter = require('./controllers/testing')
+    app.use('/api/testing', testingRouter)
+  }
+  app.use('/api/users', usersRouter);
+  
+  //private routes
+  app.use(middleware.tokenExtractor);
+  app.use(middleware.userExtractor)
+  // use the middleware only in /api/blogs routes
+  app.use('/api/blogs', blogsRouter);
+ 
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
